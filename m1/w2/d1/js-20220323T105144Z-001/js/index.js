d44a6e726287436e109e300823647a1a623ceeea
francescopasta <pastore.francesco01@gmail.com>
2022-10-31 15:50:55 +0000

    31/10 francesco_pastore

---
+++ m1/w2/d1/js-20220323T105144Z-001/js/index.js
@@ -0,0 +1,20 @@
+
+alert('Uso il pop up per mandare dei messaggi');
+window.alert('Sto studiando JS');
+
+var favouriteAnimal = "Capra"
+var favourite2Animal = "Woman"
+var anni = prompt("Quanti anni hai?")
+
+let p = document.getElementById("paragraph") 
+let btn = document.getElementById("btn")
+
+if(anni < 18){
+function save(){
+    p.innerText = favouriteAnimal + ' is my favourite animal!'
+}
+} else if (anni >= 18){
+    function save(){
+        p.innerText = favourite2Animal + ' is my favourite animal!'
+    }
+}
---
+++ m1/w2/d1/js-20220323T105144Z-001/js/naming.js
@@ -0,0 +1,18 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'Criceto';
+var PetPreferito = 'Coniglio';
+
+
+var $pet = 'Giraffa';
+console.log($pet);
+var _pet = 'Leone';
+console.log(_pet);
+var _pet2 = 'Pantera';
+document.write(_pet2);
+
+
+//var 12 = 'numero';
