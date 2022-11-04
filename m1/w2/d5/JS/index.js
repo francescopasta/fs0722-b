439c2239da7fbbf8196b0f82fed60b0246d9e97b
francescopasta <pastore.francesco01@gmail.com>
2022-11-04 19:33:37 +0000

    04/11 test_w2_francesco_pastore

--- m1/w2/d5/JS/index.js
+++ m1/w2/d5/JS/index.js
@@ -3,13 +3,6 @@ let num = document.getElementById("num")
         function aggiungiNumero(button){
             let element = button.getAttribute('data-num')
             num.value += element
-
-        if (element == " + " || element == " - "  || element == " * " || element == " / " || element == "." || element == "Math.PI "){
-
-            let stringa = num.value.element(0, num.length - 1) + e.target.innerHTML
-            num.value = stringa
-
-        }
         }
 
         function tot(){
@@ -27,51 +20,23 @@ let num = document.getElementById("num")
         }
 
         function changeOfSign(){
-
-            if(num.value > 0){ 
+            if(num.value >= 0){ 
             num.value = '- ' + num.value
-            } else {
-                num.value = '+ ' + num.value
+            } else{
+                num.value = '+ ' + eval(num.value)
             }
+        }
 
-        /*for(numero of num){
-
-            let numeri = []
-            numeri.push(numero)
-
-              if(numeri[0] > 0){
-                num.value = '- ' + num.value
-              } else if(num.value[0] < 0){
-                    numeri.forEach(element => {
-                        if(element > 0){
-                            num.value = '- ' + num.value
-                        } else {
-                            num.value = '+ ' + num.value
-                        }
-                    })
-                } else {
-
-                
-            }*/
-            
+        function funzioni(questo){
+            let it = questo.getAttribute('data-num')
+            num.value = eval(it + '(' + eval(num.value) + ')')
         }
 
         function xSup2(){
             num.value = Math.pow(eval(num.value), 2)
         }
 
-          function radice(){
-           num.value = Math.sqrt(eval(num.value))
-        }
-
-        function logaritmoE(){
-            num.value = Math.log(eval(num.value))
-        }
-
-        function logaritmo10(button){
-            num.value = Math.log10(eval(num.value))
-        }
-
         function parentesi(){
             num.value = '(' + num.value + ')'
         }
+      
