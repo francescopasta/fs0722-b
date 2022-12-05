e6a12b68fd679996a21370a3396a9aad9437c56c
francescopasta <pastore.francesco01@gmail.com>
2022-12-05 17:32:14 +0000

    _pastore 05/12

--- m2/w3/index.js
+++ m2/w3/index.js
@@ -2,21 +2,27 @@ function NamesData(name, surname, date) {
     this.name = name;
     this.surname = surname;
     this.date = date;
-    this.bornYear = this.date.substring(0,4)
-    this.bornMonth = this.date.substring(5,7)
-    this.bornDate = this.date.substring(8,10)
-    this.curDate = new Date()
-    this.curYear = this.curDate.getFullYear()
-    this.curMonth = this.curDate.getMonth()
-    this.curDay = this.curDate.getDate()
-    this.finalDate = this.curYear - this.bornYear
-    this.calc = function() {
+    this.nomCogn = function () {
+        return this.name + ' ' + this.surname
+    }
+    this.findUltimateDate = function () {
+        this.bornYear = this.date.substring(0,4)
+        this.bornMonth = this.date.substring(5,7)
+        this.bornDate = this.date.substring(8,10)
+        this.curDate = new Date()
+        this.curYear = this.curDate.getFullYear()
+        this.curMonth = this.curDate.getMonth()
+        this.curDay = this.curDate.getDate()
+        this.finalDate = this.curYear - this.bornYear
+    };
+    this.calcDate = function() {
+        this.findUltimateDate()
         if(this.bornMonth > this.curMonth){
             this.finalDate -- 
          } else if (this.curMonth == this.bornMonth && this.bornDate > this.curDay){
             this.finalDate --
          }
-    }
+    };
 } 
 
 var nome = document.querySelector('#nome')
@@ -29,10 +35,8 @@ let p = document.querySelector('p')
 bottone.addEventListener('click', () => {
     if(nome.value && data.value ){
         let newName = new NamesData(nome.value, cognome.value, data.value)
-        let nomCogn = newName.name + ' ' + newName.surname
-
-        newName.calc()
-        tBody.innerHTML += '<tr> <td class="w-25">' + nomCogn + '</td> <td class="w-25">' + newName.finalDate + '</td> </tr>'
+        newName.calcDate()
+        tBody.innerHTML += '<tr> <td class="w-25">' + newName.nomCogn() + '</td> <td class="w-25">' + newName.finalDate + '</td> </tr>'
 
         nome.value = ''
         cognome.value = ''
