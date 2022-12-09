6e0f239b8662b6948d5be5564182b410ae65ba33
francescopasta <pastore.francesco01@gmail.com>
2022-12-09 11:57:14 +0000

    francesco_pastore_esercitazione 09/12

--- m2/w3/d5/drive-download-20220407T105349Z-001/index.js
+++ m2/w3/d5/drive-download-20220407T105349Z-001/index.js
@@ -1,21 +1,19 @@
 (async function getFetch() {
-    let response = await fetch("users.json")
-    if(response){
-        let datiJson = await response.json()
-        for (let i = 0; i < datiJson.length; i++) {
-            document.querySelector('.container').innerHTML += `
-            <div class="card m-3 text-center cambiamenti" style="width: 18rem;">
-                    <div class="card-body img">
-                    <img src= ${datiJson[i].profileURL} class="card-img-top" alt="persona-logo">
-                    </div>
-                    <div class="card-body">
-                    <p class="card-text">${datiJson[i].firstName} ${datiJson[i].lastName}</p>
-                    <a href="#" class="card-link">${datiJson[i].email}</a>
-                    </div>
+    let response = await fetch("users.json").catch(function(error) {
+        console.log(error)
+    })
+    let datiJson = await response.json()
+    for (let i = 0; i < datiJson.length; i++) {
+        document.querySelector('.container').innerHTML += `
+        <div class="card m-3 text-center cambiamenti" style="width: 18rem;">
+                <div class="card-body img">
+                <img src= ${datiJson[i].profileURL} class="card-img-top" alt="persona-logo">
                 </div>
-            `
-        }
-    } else {
-        console.log("'error' - couldn't reach fetch data")
+                <div class="card-body">
+                <p class="card-text">${datiJson[i].firstName} ${datiJson[i].lastName}</p>
+                <a href="#" class="card-link">${datiJson[i].email}</a>
+                </div>
+            </div>
+        `
     }
 }) ();
