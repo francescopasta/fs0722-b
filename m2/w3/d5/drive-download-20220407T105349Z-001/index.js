d64dda0fdc3a4c9add13c609f71f2d3c9ceb727b
francescopasta <pastore.francesco01@gmail.com>
2022-12-09 11:44:15 +0000

    francesco_pastore _esercitazione 09/12

--- m2/w3/d5/drive-download-20220407T105349Z-001/index.js
+++ m2/w3/d5/drive-download-20220407T105349Z-001/index.js
@@ -1,17 +1,21 @@
 (async function getFetch() {
     let response = await fetch("users.json")
-    var datiJson = await response.json()
-    for (let i = 0; i < datiJson.length; i++) {
-        document.querySelector('.container').innerHTML += `
-        <div class="card m-3 text-center cambiamenti" style="width: 18rem;">
-                <div class="card-body img">
-                <img src= ${datiJson[i].profileURL} class="card-img-top" alt="persona-logo">
+    if(response){
+        let datiJson = await response.json()
+        for (let i = 0; i < datiJson.length; i++) {
+            document.querySelector('.container').innerHTML += `
+            <div class="card m-3 text-center cambiamenti" style="width: 18rem;">
+                    <div class="card-body img">
+                    <img src= ${datiJson[i].profileURL} class="card-img-top" alt="persona-logo">
+                    </div>
+                    <div class="card-body">
+                    <p class="card-text">${datiJson[i].firstName} ${datiJson[i].lastName}</p>
+                    <a href="#" class="card-link">${datiJson[i].email}</a>
+                    </div>
                 </div>
-                <div class="card-body">
-                <p class="card-text">${datiJson[i].firstName} ${datiJson[i].lastName}</p>
-                <a href="#" class="card-link">${datiJson[i].email}</a>
-                </div>
-            </div>
-        `
+            `
+        }
+    } else {
+        console.log("'error' - couldn't reach fetch data")
     }
 }) ();
