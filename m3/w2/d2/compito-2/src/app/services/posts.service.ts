ade2c2302e57a5a0d6303c0fb049e7b212a788b4
francescopasta <pastore.francesco01@gmail.com>
2023-01-10 18:13:49 +0100

    francesco 10/01

--- m3/w2/d2/compito-2/src/app/services/posts.service.ts
+++ m3/w2/d2/compito-2/src/app/services/posts.service.ts
@@ -10,15 +10,10 @@ export class PostsService {
 
   constructor() { }
 
-  async getPost(){
-    fetch('/../../assets/db.json')
-    .then(data => {
-    return data.json();
-    }).then(res => {
-      for (let i = 0; i < res.length; i++) {
-        this.posts.push(res[i])
-      }
-      return this.posts
+  getPosts(a){
+    fetch('assets/db.json').then(data => { return data.json();})
+    .then(res => {
+      res.filter(p=> a)
     })
   }
 }
