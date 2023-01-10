4bc04f8ceab364cafb9ee5bbfe3f340605655b1b
francescopasta <pastore.francesco01@gmail.com>
2023-01-10 18:14:18 +0100

    francesco 10/01 compito

--- m3/w2/d2/compito-2/src/app/app-routing.module.ts
+++ m3/w2/d2/compito-2/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PostAttiviComponent } from './components/post-attivi/post-attivi.component';
+import { PostUnactiveComponent } from './components/post-unactive/post-unactive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'actives',
+    component: PostAttiviComponent
+  },
+  {
+    path: 'unactives',
+    component: PostUnactiveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
