a29fb31e7938fef10f34d21cf979b986b1bc645d
francescopasta <pastore.francesco01@gmail.com>
2023-03-03 16:58:54 +0100

    francesco_pastore_esercitazione_w1_d5_m2

---
+++ back-end/m2/w1/d5/gestionePrenotazioni/src/main/java/com/example/gestionePrenotazioni/GestionePrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionePrenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionePrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionePrenotazioniApplication.class, args);
+	}
+
+}
---
+++ back-end/m2/w1/d5/gestionePrenotazioni/src/test/java/com/gestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
